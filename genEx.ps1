e4d0669368dd8471a903ab5bffd7b6216be27e19
Nima Bargestan <nimam700@gmail.com>
2023-06-26 21:28:17 -0400

    Add small texts as loading state and create package.json

--- genEx.ps1
+++ genEx.ps1
@@ -1,17 +1,25 @@
 param (
-    [Parameter(Mandatory=$true, Position=0)]
+    [Parameter(Mandatory = $true, Position = 0)]
     [Alias("d", "dir", "directory")]
     [string]$directoryName
 )
 
 # Create the directory if it doesn't exist
 if (-not (Test-Path -Path $directoryName -PathType Container)) {
+    Write-Host "[*] Creating directory => '$directoryName' "
     New-Item -ItemType Directory -Name $directoryName -Path "./" -ErrorAction SilentlyContinue | Out-Null
-} else {
-    Write-Host "The directory '$directoryName' already exists."
+    Write-Host "[+] Directory ('$directoryName') Created Successfully.`n"
+}
+else {
+    Write-Host "[-] Directory ('$directoryName') already exists!"
     return
 }
 
 Set-Location $directoryName
 
+# Create package.json file
+Write-Host "[*] Creating => package.json ..."
 
+Invoke-Expression -Command "npm init -y" | Out-Null
+
+Write-Host "[+] File (package.json) Created Successfully.`n"

e7960db1f1a09ddd11d7c323d9817e3e12d9d78f
Nima Bargestan <nimam700@gmail.com>
2023-06-26 21:37:22 -0400

    Create git init logic

--- genEx.ps1
+++ genEx.ps1
@@ -7,7 +7,7 @@ param (
 # Create the directory if it doesn't exist
 if (-not (Test-Path -Path $directoryName -PathType Container)) {
     Write-Host "[*] Creating directory => '$directoryName' "
-    New-Item -ItemType Directory -Name $directoryName -Path "./" -ErrorAction SilentlyContinue | Out-Null
+    New-Item -ItemType Directory -Name $directoryName -Path "./" | Out-Null
     Write-Host "[+] Directory ('$directoryName') Created Successfully.`n"
 }
 else {
@@ -18,8 +18,16 @@ else {
 Set-Location $directoryName
 
 # Create package.json file
-Write-Host "[*] Creating => package.json ..."
+Write-Host "[*] Creating file => package.json"
 
 Invoke-Expression -Command "npm init -y" | Out-Null
 
 Write-Host "[+] File (package.json) Created Successfully.`n"
+
+# init git
+Write-Host "[*] Initializing => Git"
+
+Invoke-Expression -Command "git init" | Out-Null
+New-Item -ItemType File -Name ".gitignore" -Path "./" | Out-Null
+
+Write-Host "[+] Git Initialized Successfully.`n"
